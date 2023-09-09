22116698c8eb274ad49a142a747c93a2b260a42d
Alexander Zagniotov <azagniotov@gmail.com>
2023-09-09 20:49:56 +0900

    Building user dictionary from a Gradle task (#28)

--- src/main/java/io/github/azagniotov/lucene/analysis/ja/sudachi/cache/DictionaryCache.java
+++ src/main/java/io/github/azagniotov/lucene/analysis/ja/sudachi/cache/DictionaryCache.java
@@ -1,8 +1,6 @@
 package io.github.azagniotov.lucene.analysis.ja.sudachi.cache;
 
 import com.worksap.nlp.sudachi.Dictionary;
-import com.worksap.nlp.sudachi.dictionary.UserDictionaryBuilder;
-import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -12,17 +10,6 @@ public enum DictionaryCache {
     private static final String CACHE_KEY = "dictionary";
     public static final String SYSTEM_DICT_LOCAL_PATH = "/tmp/sudachi/system-dict/system_core.dic";
     public static final String USER_DICT_LOCAL_PATH = "/tmp/sudachi/user_lexicon.dic";
-
-    static {
-        // TODO To move to Gradle?
-        try {
-            final String userLexiconCsvPath = "/tmp/sudachi/user_lexicon.csv";
-            UserDictionaryBuilder.main(
-                    new String[] {"-o", USER_DICT_LOCAL_PATH, "-s", SYSTEM_DICT_LOCAL_PATH, userLexiconCsvPath});
-        } catch (IOException e) {
-            throw new IllegalArgumentException("Failed to create JapaneseDictionary instance", e);
-        }
-    }
 
     private final Map<String, Dictionary> dictionaryCache;
 
